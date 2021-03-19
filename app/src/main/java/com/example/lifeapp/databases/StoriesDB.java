fd3887d564d20dac6f59650bdf03d273df8a1a9c
Omar Yousef <w0753671@myscc.ca>
2021-03-19 00:25:24 -0400

    Created the StoriesDB class that is going to include all the Story's CRUD

---
+++ app/src/main/java/com/example/lifeapp/databases/StoriesDB.java
@@ -0,0 +1,12 @@
+package com.example.lifeapp.databases;
+
+/**
+ * @author Omar Yousef
+ * @version 1.0
+ * @date March 19th 2021
+ *
+ * StoriesDB class:
+ * This class will have all the CRUD operations for the stories table
+ */
+public class StoriesDB {
+}
