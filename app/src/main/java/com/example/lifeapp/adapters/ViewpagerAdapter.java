f2098add42f91b261ac8d657969c2b9aa9e2185c
Omar Yousef <w0753671@myscc.ca>
2021-03-19 00:24:43 -0400

    Created the adapters that we will need for the project

---
+++ app/src/main/java/com/example/lifeapp/adapters/ViewpagerAdapter.java
@@ -0,0 +1,12 @@
+package com.example.lifeapp.adapters;
+
+/**
+ * @author Omar Yousef
+ * @version 1.0
+ * @date March 19th 2021
+ *
+ * ViewPager Adapter class:
+ * This class will be the adapter for the home page's viewpager
+ */
+public class ViewpagerAdapter {
+}
