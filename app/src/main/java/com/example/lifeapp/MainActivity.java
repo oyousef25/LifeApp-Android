aa8bc06bf16fee7875b2d0d3d18f9c9847408459
Omar Yousef <w0753671@myscc.ca>
2021-03-19 00:22:22 -0400

    Finished setting up the project's theme and the basic setups

--- app/src/main/java/com/example/lifeapp/MainActivity.java
+++ app/src/main/java/com/example/lifeapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.nav_home, R.id.nav_stories, R.id.nav_form, R.id.nav_weather, R.id.nav_account)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
