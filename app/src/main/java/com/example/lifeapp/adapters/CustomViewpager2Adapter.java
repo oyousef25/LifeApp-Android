847eee1d267005d64b02cabf44c97e0d18e34ab7
Omar Yousef <w0753671@myscc.ca>
2021-03-20 17:51:04 -0400

    Created the ViewpagerAdapter.java

---
+++ app/src/main/java/com/example/lifeapp/adapters/CustomViewpager2Adapter.java
@@ -0,0 +1,45 @@
+package com.example.lifeapp.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.lifeapp.R;
+import com.example.lifeapp.fragments.HomeViewpagerFragment;
+
+/**
+ * @author Omar Yousef
+ * @version 1.0
+ * @date March 20th 2021
+ *
+ * CustomViewpager2 Adapter:
+ * It is going to be the adapter for our home page viewpager
+ */
+public class CustomViewpager2Adapter extends FragmentStateAdapter {
+    public CustomViewpager2Adapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return HomeViewpagerFragment.newInstance(R.drawable.pager_recipes);
+            case 1:
+                return HomeViewpagerFragment.newInstance(R.drawable.pager_exercises);
+            case 2:
+                return HomeViewpagerFragment.newInstance(R.drawable.pager_locations);
+            case 3:
+                return HomeViewpagerFragment.newInstance(R.drawable.pager_stories);
+            default:
+                return HomeViewpagerFragment.newInstance(R.drawable.pager_exercises);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
