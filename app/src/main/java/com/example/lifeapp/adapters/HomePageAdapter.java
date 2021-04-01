0d0406dbf52a7cda919bd20a9dd8c74f619ad965
Omar Yousef <w0753671@myscc.ca>
2021-04-01 18:09:10 -0400

    Created the recyclerview's adapter

--- app/src/main/java/com/example/lifeapp/adapters/HomePageAdapter.java
+++ app/src/main/java/com/example/lifeapp/adapters/HomePageAdapter.java
@@ -1,5 +1,20 @@
 package com.example.lifeapp.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lifeapp.R;
+import com.example.lifeapp.pojo.CategoryItem;
+
+import java.util.ArrayList;
+
 /**
  * @author Omar Yousef
  * @version 1.0
@@ -8,5 +23,86 @@ package com.example.lifeapp.adapters;
  * HomePageAdapter class:
  * This class will be the adapter for the home page's recyclerView.
  */
-public class HomePageAdapter {
+public class HomePageAdapter extends RecyclerView.Adapter<HomePageAdapter.CustomViewHolder>{
+
+    //Properties
+    private ArrayList<CategoryItem> categoryItems;
+    private Context context;
+
+    /**
+     * HomePageAdapter constructor
+     * @param categoryItems
+     * @param context
+     */
+    public HomePageAdapter(ArrayList<CategoryItem> categoryItems, Context context) {
+        this.categoryItems = categoryItems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Creating a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_home_recyclerview, null);
+
+        //Returning our view
+        return new CustomViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+        //Creating a new category item using the our categoryItems arrayList
+        CategoryItem categoryItem = categoryItems.get(position);
+
+        //Retrieving the properties data using the position and assigning them to our object's properties
+        holder.image.setImageResource(categoryItem.getImage());
+        holder.title.setText(categoryItem.getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        //Returning the count of the arrayList's length
+        if (categoryItems != null){
+            return categoryItems.size();
+        }else{
+            return 0;
+        }
+    }
+
+
+
+    /**
+     * @author Omar Yousef
+     * @version 1.0
+     * @date April 1st 2021
+     *
+     * CustomViewHolder()
+     * CustomViewHolder class that we are going to use in our adapter
+     */
+    class CustomViewHolder extends RecyclerView.ViewHolder{
+
+        //Properties
+        protected ImageView image;
+        protected TextView title;
+
+        /**
+         * @author Omar Yousef
+         * @version 1.0
+         * @date April 1st 2021
+         * @param itemView
+         *
+         * The CustomViewHolder constructor
+         */
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //Connecting our properties with the UI elements in our XML layout file
+            this.image = itemView.findViewById(R.id.homeImage);
+            this.title = itemView.findViewById(R.id.homeTitle);
+
+
+        }
+    }
 }
