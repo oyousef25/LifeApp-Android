e03aec545498b020962a5020ea507f1a0d01c4a9
Omar Yousef <w0753671@myscc.ca>
2021-04-01 22:13:31 -0400

    Finished the WeatherDB

--- app/src/main/java/com/example/lifeapp/databases/StoriesDB.java
+++ app/src/main/java/com/example/lifeapp/databases/StoriesDB.java
@@ -9,4 +9,28 @@ package com.example.lifeapp.databases;
  * This class will have all the CRUD operations for the stories table
  */
 public class StoriesDB {
+    //DB version
+    public static final int DATABASE_VERSION = 1;
+
+    //DB name
+    public static final String DATABASE_NAME = "lifeapp";
+
+    //Table name
+    public static final String TABLE_stories = "stories";
+
+    /*
+        Column names
+     */
+    public static final String COLUMN_ID = "id";
+
+    /*
+        Stories Table
+     */
+    //COLUMNS
+//    public static final String COLUMN_TITLE = "city"; //story title
+//    public static final String COLUMN_DESCRIPTION = "temperature"; //story description
+
+
+    //CREATE TABLE
+//    public static final String CREATE_STORIES_TABLE =
 }
