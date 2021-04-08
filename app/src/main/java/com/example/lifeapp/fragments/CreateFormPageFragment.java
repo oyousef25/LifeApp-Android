97773162c4ce8089911ece360ad7b415efa3ac97
Omar Yousef <w0753671@myscc.ca>
2021-04-07 20:13:47 -0400

    Finished The form XML layout file

--- app/src/main/java/com/example/lifeapp/fragments/CreateFormPageFragment.java
+++ app/src/main/java/com/example/lifeapp/fragments/CreateFormPageFragment.java
@@ -64,6 +64,9 @@ public class CreateFormPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_form_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_form_page, container, false);
+
+        //Return view
+        return view;
     }
 }
